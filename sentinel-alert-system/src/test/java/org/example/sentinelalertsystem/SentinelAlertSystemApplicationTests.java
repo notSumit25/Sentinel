aca0e6bcd59c5576b66f805acc98cf6a25efaffd
Lernobug <gunjanaggarwal12345.com>
2026-01-15 13:53:01 +0530

    alert system

---
+++ sentinel-alert-system/src/test/java/org/example/sentinelalertsystem/SentinelAlertSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sentinelalertsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelAlertSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
